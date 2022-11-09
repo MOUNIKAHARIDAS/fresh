acbcab2b1d157af65e86fd4a78c00053b9a0be05
mounika <haridasmounika@gmail.com>
2022-11-09 12:42:14 -0500

    messge

---
+++ A6.java
@@ -0,0 +1,38 @@
+class A6
+{
+	int m1()
+	{
+		int i=6894;
+		 return i;
+	}
+	String  m2()
+	{
+		return "vamshi";
+	}
+
+	double m3()
+	{
+		double d=6364;
+
+		return d;
+
+	}
+	short m5()
+	{
+		return 89;
+	}
+	public static void main(String args[])
+	{
+
+		A6 a=new A6();
+		 int p=a.m1();
+		 String q=a.m2();
+		 double r=a.m3();
+		 short  s=a.m5();
+
+		 System.out.println(p);
+         System.out.println(q);
+         System.out.println(r);
+         System.out.println(s);
+     }
+ }
